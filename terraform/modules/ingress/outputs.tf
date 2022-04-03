dfbd520cf6e0ec46b450d37f1213d5f8f4a3a386
Michael Mosher <michaelm@auctionedge.com>
2022-04-03 10:31:06 -0400

    Integrate compute and ingress modules

--- terraform/modules/ingress/outputs.tf
+++ terraform/modules/ingress/outputs.tf
@@ -9,3 +9,7 @@ output "load_balancer_dns" {
 output "load_balancer_listener_arn" {
   value = { for k, v in aws_lb_listener.main : k => v.arn }
 }
+
+output "load_balancer_security_group_id" {
+  value = aws_security_group.main.id
+}
--- terraform/us-east-1/foundations/main.tf
+++ terraform/us-east-1/foundations/main.tf
@@ -8,10 +8,11 @@ module "network" {
 module "compute" {
   source = "../../modules/compute"
 
-  cluster_identifier    = "east-1"
-  cloud_init_files_path = local.cloud_init_files_path
-  cluster_subnet_ids    = toset(module.network.private_subnet_ids)
-  vpc_id                = module.network.vpc_id
+  cluster_identifier              = "east-1"
+  cloud_init_files_path           = local.cloud_init_files_path
+  cluster_ingress_security_groups = [module.ingress.load_balancer_security_group_id]
+  cluster_subnet_ids              = toset(module.network.private_subnet_ids)
+  vpc_id                          = module.network.vpc_id
 
   controller_instance_profile = "cluster-controller"
   controller_instance_type    = "t2.small"
@@ -30,4 +31,36 @@ module "ingress" {
   load_balancer_subnet_ids = toset(module.network.public_subnet_ids)
   route53_public_zone_id   = data.aws_route53_zone.public.zone_id
   vpc_id                   = module.network.vpc_id
+}
+
+resource "aws_lb_listener_rule" "consul" {
+  listener_arn = module.ingress.load_balancer_listener_arn["consul"]
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = module.compute.consul_target_group_arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "nomad" {
+  listener_arn = module.ingress.load_balancer_listener_arn["nomad"]
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = module.compute.nomad_target_group_arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
 }
