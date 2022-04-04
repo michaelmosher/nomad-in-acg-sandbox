7e9b6869cfa70922b1d656c851b3a5b8a9c681be
Michael Mosher <michaelm@auctionedge.com>
2022-04-04 07:45:56 -0400

    Give cluster-works permissions required for CTS module

--- terraform/global/ec2_cluster_worker_role.tf
+++ terraform/global/ec2_cluster_worker_role.tf
@@ -13,3 +13,48 @@ resource "aws_iam_role_policy" "worker_cloud_auto_join" {
   role   = aws_iam_role.ec2_cluster_worker.name
   policy = data.aws_iam_policy_document.describe_instances.json
 }
+
+resource "aws_iam_role_policy" "cts" {
+  name = "cts-upate-load-balancer"
+  role = aws_iam_role.ec2_cluster_worker.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          // CRUD listener rules
+          "elasticloadbalancing:CreateRule",
+          "elasticloadbalancing:ModifyRule",
+          "elasticloadbalancing:DeleteRule",
+          // CRUD target groups
+          "elasticloadbalancing:CreateTargetGroup",
+          "elasticloadbalancing:ModifyTargetGroup",
+          "elasticloadbalancing:DeleteTargetGroup",
+          // CRUD target group attachments
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:elasticloadbalancing:*:*:listener/net/*/*/*",
+          "arn:aws:elasticloadbalancing:*:*:listener/app/*/*/*",
+          "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+          "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*/*/*",
+          "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*/*/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances", # you say redundant, I say independent
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeLoadBalancerAttributes",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeRules",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
