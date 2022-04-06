fb5279110bb688cac48c75e166e99af0e854a575
Michael Mosher <michaelm@auctionedge.com>
2022-04-06 14:24:28 -0400

    Give cluster workers EFS permissions.
    These will be required for the CSI driver to manage EFS access points
    for our Nomad tasks.
    
    Reference: https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/docs/iam-policy-example.json

--- terraform/global/ec2_cluster_worker_role.tf
+++ terraform/global/ec2_cluster_worker_role.tf
@@ -13,3 +13,25 @@ resource "aws_iam_role_policy" "worker_cloud_auto_join" {
   role   = aws_iam_role.ec2_cluster_worker.name
   policy = data.aws_iam_policy_document.describe_instances.json
 }
+
+resource "aws_iam_role_policy" "worker_efs" {
+  name = "aws-efs-csi-driver"
+  role = aws_iam_role.ec2_cluster_worker.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "elasticfilesystem:DescribeAccessPoints",
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:DescribeMountTargets",
+          "elasticfilesystem:CreateAccessPoint",
+          "elasticfilesystem:DeleteAccessPoint",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
